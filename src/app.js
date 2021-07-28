6918667cc5ff246853390cd5c7e2116fd07ec18f
unknown <harshitsainiboy18@gmail.com>
2021-07-28 14:35:38 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const root_path = path.join(__dirname, '../public/project2')
+
+app.use(express.static(root_path))
+
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+})
+
+
+
+app.listen(port, () => {
+    console.log(`your server is running on ${port}`);
+})
